52ad555ed90eabd50471d5f185e049c37ab9bbc0
Dana Merrick <dmerrick@dmerrick-dt-2.local>
2009-06-10 14:45:28 -0500

    Added a little object orientation.

--- set_desktop.rb
+++ set_desktop.rb
@@ -1,6 +1,16 @@
 #!/usr/bin/env ruby -wKU
 
-p DATA.readlines
+class SetDesktop
+  def create_image_from_DATA
+    p DATA.readlines
+  end
+end
+
+if $0 == __FILE__
+  desk = SetDesktop.new
+
+  desk.create_image_from_DATA
+end
 
 __END__
 data!
